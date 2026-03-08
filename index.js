bf424025c1ebed6cdb0dc6587c760b6004a320bd
T.Mabotja <mabatshegofatso842@gmail.com>
2026-03-08 23:57:00 +0200

    fixing logical errors on my if statement

--- index.js
+++ index.js
@@ -73,16 +73,17 @@ document.getElementById(`Reset`).onclick=function(){
 
  //if statements
  let randomNum= Math.ceil(Math.random()*10);
- let usernum= document.getElementById("guess").value;
 
  document.getElementById("Title").textContent= `Guess a number between 0 and 10`
+ document.getElementById("num").textContent = ``;
 
  document.getElementById("Submit").onclick=function(){
-   document.getElementById("num").textContent = randomNum;
-   if(randomNum>usernum){
+   let usernum= document.getElementById("guess").value;
+
+   if(randomNum > usernum){
       document.getElementById("num").textContent=`the random number is ${randomNum}, your guess is low`
    }
-   else if(randomNum===usernum){
+   else if(randomNum === usernum){
       document.getElementById("num").textContent=`Your number is the correct guess`
    }
    else{
