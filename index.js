c127a28fb2be6b531ef042e36207fdb882a2773d
T.Mabotja <mabatshegofatso842@gmail.com>
2026-03-09 01:48:38 +0200

    changed the random guesser

--- index.js
+++ index.js
@@ -72,12 +72,12 @@ document.getElementById(`Reset`).onclick=function(){
  //document.getElementById(`RandomInt`).textContent= x;
 
  //if statements
- let randomNum= Math.ceil(Math.random()*10);
 
  document.getElementById("Title").textContent= `Guess a number between 0 and 10`
  document.getElementById("num").textContent = ``;
 
  document.getElementById("Submit").onclick=function(){
+    let randomNum= Math.ceil(Math.random()*10);
    let usernum= document.getElementById("guess").value;
 
    if(randomNum > usernum){
