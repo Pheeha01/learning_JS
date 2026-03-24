6832670b35b5e35a55ee1ba7151641077566425b
T.Mabotja <mabatshegofatso842@gmail.com>
2026-03-24 22:43:04 +0200

    adding and removing pictures to favourites section

--- manipulatingPictures/mandela.js
+++ manipulatingPictures/mandela.js
@@ -1,10 +1,19 @@
 let mandela = document.querySelectorAll("#pics > .mandela");
+let picDiv= document.querySelector("#pics")
+let favourites = document.querySelector("fieldset");
+favourites.style.display = "flex"
+favourites.style.gap= "5px"
 
-let favourites = document.querySelector("#favourites");
-
-
-for(i =0; i < mandela.length; i++){
-    mandela[i].addEventListener('click', function(){
-        favourites.appendChild(mandela[i])
-    })
-}
+mandela.forEach(picture => {
+    picture.addEventListener('click', function(){
+        if (!favourites.contains(picture)){
+            favourites.appendChild(picture)
+            picture.style.width = "120px";
+            picture.style.height = "120px"
+            picture.style.borderRadius = "20px"
+        }
+        else{
+            picDiv.appendChild(picture)
+        }
+    })  
+})
